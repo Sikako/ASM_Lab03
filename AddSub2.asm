39a82c073fab579490fe2baf73395996308f9731
Sikako <71139205+Sikako@users.noreply.github.com>
2022-02-23 09:38:39 +0800

    費式數列

--- AddSub2.asm
+++ AddSub2.asm
@@ -7,17 +7,34 @@ INCLUDE Irvine32.inc
 
 .data
 
-	FIBNum    DWORD 100 DUP(?)
+	FIBNum    DWORD 30 DUP(0)
 
 
 .code
 main PROC
 
-	mov esi, OFFSET FIBNum
-	mov FIBNum, 1
-	mov FIBNum+4, 1
-	mov ax, [esi+4]
-	call	DumpRegs			; display the registers
+	mov  FIBNum, 1
+	mov  FIBNum+4, 1
+	mov  ecx, 28
+	mov  ebx, 0
+	mov  esi, 0
+
+	mov  eax, 1
+	call WriteInt
+	call CRLF
+	call WriteInt
+	call CRLF
+
+L1:
+	add  ebx, [FIBNum + 4 + esi]
+	add  ebx, [FIBNum + esi]
+	mov  FIBNum[8 + esi], ebx
+	mov  eax, ebx
+	call WriteInt 					; print out
+	call CRLF						; \n
+	xor  ebx, ebx
+	add  esi, 4
+	loop L1
 
 	exit
 main ENDP
